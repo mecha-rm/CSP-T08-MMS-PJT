d28cb70f00098596282984e0d8ec77c04e64dd37
Roderick Montague <roderick.montague243@gmail.com>
2022-02-15 15:51:22 -0500

    Made Item Script
    Made an item script for the game. No functionality as of yet.

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// an item in the game world.
+// TODO: maybe make an inventory script?
+public class Item : MonoBehaviour
+{
+    // the object in the game world that represents this item.
+    public GameObject worldObject;
+
+    // the icon for this item in the user interface.
+    public GameObject icon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
