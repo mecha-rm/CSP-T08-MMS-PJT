f1a2ad79c4726c70a8dd50fd079dadc7fd1307d4
Roderick Montague <roderick.montague243@gmail.com>
2022-04-01 09:35:41 -0400

    Replaced with the Original Script
    I replaced it with the original script since that one worked.

--- Assets/Scripts/Utilities/CopyObject.cs
+++ Assets/Scripts/Utilities/CopyObject.cs
@@ -3,7 +3,6 @@
  * https://docs.unity3d.com/ScriptReference/ExecuteInEditMode.html
  * https://docs.unity3d.com/ScriptReference/ExecuteAlways.html
  * https://www.youtube.com/watch?v=EFSXZO32h_0
- * https://docs.unity3d.com/ScriptReference/Object.DestroyImmediate.html
  */
 using System.Collections;
 using System.Collections.Generic;
@@ -35,22 +34,13 @@ public class CopyObject : MonoBehaviour
     [Tooltip("Generates copies when the program starts. This is set to 'false' when the Start() function is finished.")]
     public bool copyOnStart = true;
 
-    // editor parameters.
+    // the editor variables.
     [Header("Editor")]
 
     // if 'true', the copies are made in edit mode. This acts a button of sorts, and forces 'copyOnStart' to be true until it's done.
     [Tooltip("A button that generates copies in edit mode. This forces copyOnStart to be active until the copies are made."
         + "The copies must also be manually deleted by the user.")]
     public bool copyInEditMode = false;
-
-    // the preview objects.
-    [Tooltip("List of preview objects that are added or deleted in accordance with the preview parameter.")]
-    public List<CopyObject> previewObjects = new List<CopyObject>();
-    
-    // if 'true',  preview is made.
-    [Tooltip("Preview the configuration. Any objects made while preview is on will be deleted when the application goes into play mode." +
-        " To refresh the preview, turn preview off and then back on.")]
-    public bool preview = false;
 
     [Header("Iterations")]
 
@@ -87,18 +77,8 @@ public class CopyObject : MonoBehaviour
     void Start()
     {
         // copy the object in edit mode, or if the application is playing.
-        if(copyInEditMode || Application.isPlaying)
+        if (copyInEditMode || Application.isPlaying)
         {
-            // if the application is now playing, destroy the preview objects.
-            if (Application.isPlaying)
-            {
-                // destroys the preview objects and sets preview to false.
-                preview = false;
-                DestroyPreviewObjects();
-            }
-                
-
-
             // should not make more copies once this runs once in edit mode.
             copyInEditMode = false;
 
@@ -116,7 +96,7 @@ public class CopyObject : MonoBehaviour
                 GenerateCopies();
             }
         }
-        
+
     }
 
 
@@ -176,7 +156,7 @@ public class CopyObject : MonoBehaviour
         CopyObject copy = Instantiate(this, transform.position, transform.rotation);
 
         // if 'true', it deactivates the copy.
-        if(deactivateCopy)
+        if (deactivateCopy)
             copy.gameObject.SetActive(false);
 
         // this is the original.
@@ -232,115 +212,31 @@ public class CopyObject : MonoBehaviour
         if (totalIters <= 0)
             return;
 
-        // saves the original preview value.
-        // this is to prevent infinite copies.
-        bool isPreview = preview;
-        preview = false;
-
         // goes through all original objects.
-        for(int i = 1; i <= totalIters; i++)
+        for (int i = 1; i <= totalIters; i++)
         {
             // generates a copy.
             CopyObject copy = GenerateCopy(i, deactivateAll);
-
-            // if this is in preview mode, add the preview object to the list.
-            if (isPreview)
-                previewObjects.Add(copy);
         }
-
-        // restore the value.
-        preview = isPreview;
-    }
-
-    // clears out missing preview objects.
-    public void ClearNullPreviewObjects()
-    {
-        // clears out all null preview object.
-        for (int i = previewObjects.Count - 1; i >= 0; i--)
-        {
-            // destroies the objects.
-            if (previewObjects[i] == null)
-            {
-                previewObjects.RemoveAt(i);
-            }
-        }
-    }
-
-    // destroys copies in the preview. This is only for objects made in the editor.
-    public void DestroyPreviewObjects()
-    {
-        // destorys all preview object.
-        for(int i = 0; i < previewObjects.Count; i++)
-        {
-            // destroies the objects.
-            if (previewObjects[i] != null)
-            {
-                // check if in editor or not.
-                // if not in the editor, call regular destroy.
-                if (Application.isEditor)
-                    DestroyImmediate(previewObjects[i].gameObject);
-                else if(Application.isPlaying)
-                    Destroy(previewObjects[i].gameObject);
-            }
-        }
-
-        // clears out the list.
-        previewObjects.Clear();
     }
 
     // Update is called once per frame
     private void Update()
     {
         // recalls the start function if this should run in edit mode.
-        if(Application.isEditor)
+        if (Application.isEditor && copyInEditMode)
         {
-            // if in preview mode, and attempting to copy in the editor at the same time.
-            if(preview && copyInEditMode)
-            {
-                // the user can't make copies while in edit mode.
-                // I did this since the edit mode objects would be counted as preview objects anyway.
-                copyInEditMode = false;
-                Debug.LogWarning("Parameter copyInEditMode cannot be used in the Inspector in preview mode.");
-            }
+            // saves the original start value.
+            bool startOrig = copyOnStart;
 
-            // if there are preview objects, remove empty ones.
-            if (previewObjects.Count != 0)
-                ClearNullPreviewObjects();
+            // since this is running in the editor, this should be set to true.
+            copyOnStart = true;
 
-            // if in preview mode, create someo objects.
-            // if not in preview mode and there are preview objects, destroy them.
-            if (preview && previewObjects.Count == 0)
-            {
-                // to avoid an infinite loop, this requires the list to be empty.
-                copyInEditMode = true;
-            }
-            else if (!preview && previewObjects.Count != 0)
-            {
-                DestroyPreviewObjects();
-            }
-                
-            // should make copies in edit mode.
-            if(copyInEditMode)
-            {
-                // saves the original start value.
-                bool startOrig = copyOnStart;
+            // calls Start() to set things up.
+            Start();
 
-                // since this is running in the editor, this should be set to true.
-                copyOnStart = true;
-
-                // calls Start() to set things up.
-                Start();
-
-                // restores it so that it's set for next time.
-                copyOnStart = startOrig;
-            }
-            
-        }
-        // some preview objects would not get deleted, so this gets rid of them.
-        else if(Application.isPlaying && previewObjects.Count != 0)
-        {
-            preview = false;
-            DestroyPreviewObjects();
+            // restores it so that it's set for next time.
+            copyOnStart = startOrig;
         }
     }
 }
